08faa04f61adfd87f8e02ceb2b8d3408737ff02e
hayfield <hayfield@gmx.com>
2012-10-08 19:14:09 +0100

    Create a basic environment that does nothing useful

--- src/js/environment/environment.js
+++ src/js/environment/environment.js
@@ -1,4 +1,12 @@
 Pointerly.Environment = function(){
+	Pointerly.CURRENT_ENVIRONMENT = this;
+
+	this.scene = new THREE.Scene();
+	this.renderer = Detector.webgl ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer;
+	this.renderer.setSize( 300, 300 );
+	document.body.appendChild( this.renderer.domElement );
+	this.rendererContext = this.renderer.domElement.getContext('experimental-webgl');
+
 	console.log('created env');
 };
 
