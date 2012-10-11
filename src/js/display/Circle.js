dfdb1c891c29e70504523e336a4cce52756f7096
hayfield <hayfield@gmx.com>
2012-10-11 17:35:48 +0100

    Add a circle shape

---
+++ src/js/display/Circle.js
@@ -0,0 +1,20 @@
+Pointerly.Circle = function(){
+	Pointerly.Shape.call( this );
+
+	this.setColor( 'orange' );
+
+	this.draw = function( canvas ){
+		var width = canvas.width,
+			height = canvas.height,
+			ctx = canvas.getContext('2d');
+
+		ctx.beginPath();
+		ctx.arc( width/2, height/2, Math.min(width/2, height/2), 0, Math.PI*2, true );
+		ctx.fill();
+	};
+
+	this.display( this.draw );
+};
+
+Pointerly.Circle.prototype = new Pointerly.Shape;
+Pointerly.Circle.prototype.constructor = Pointerly.Circle;
