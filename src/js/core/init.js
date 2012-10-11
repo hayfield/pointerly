01a15c69daf126a3f5266416ed56ebb56085212c
hayfield <hayfield@gmx.com>
2012-10-11 13:41:24 +0100

    Change the size of the renderer when the window size changes

--- src/js/core/init.js
+++ src/js/core/init.js
@@ -2,3 +2,9 @@ Pointerly.Init = function(){
 	if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 	var env = new Pointerly.Environment();
 };
+
+window.onresize = function(){
+	if( Pointerly.CURRENT_ENVIRONMENT instanceof Pointerly.Environment ){
+		Pointerly.CURRENT_ENVIRONMENT.renderer.setSize( window.innerWidth, window.innerHeight );
+	}
+}
