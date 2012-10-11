30f5229937057f82656c4de5b007f17d43860e34
hayfield <hayfield@gmx.com>
2012-10-11 17:10:07 +0100

    Indent and better name variables passed to the magic function

--- src/js/display/Shape.js
+++ src/js/display/Shape.js
@@ -20,23 +20,22 @@ Pointerly.Shape = function(){
 		shape.texture.needsUpdate = true;
 	};
 
-	this.draw(function(){
-	var canvas = arguments[0];
-	var ctx = canvas.getContext('2d');
-	ctx.fillStyle = "orange";
-	ctx.fillRect(0,0,512,512);
-	ctx.fillStyle = "red";
-	ctx.fillRect(25,25,220,220);
-	ctx.fillStyle = "blue";
-	ctx.beginPath();
-	ctx.arc(75,75,50,0,Math.PI*2,true); // Outer circle
-	ctx.moveTo(110,75);
-	ctx.arc(75,75,35,0,Math.PI,false);   // Mouth (clockwise)
-	ctx.moveTo(65,65);
-	ctx.arc(60,65,5,0,Math.PI*2,true);  // Left eye
-	ctx.moveTo(95,65);
-	ctx.arc(90,65,5,0,Math.PI*2,true);  // Right eye
-	ctx.stroke();
+	this.draw(function( canvas ){
+		var ctx = canvas.getContext('2d');
+		ctx.fillStyle = "orange";
+		ctx.fillRect(0,0,512,512);
+		ctx.fillStyle = "red";
+		ctx.fillRect(25,25,220,220);
+		ctx.fillStyle = "blue";
+		ctx.beginPath();
+		ctx.arc(75,75,50,0,Math.PI*2,true); // Outer circle
+		ctx.moveTo(110,75);
+		ctx.arc(75,75,35,0,Math.PI,false);   // Mouth (clockwise)
+		ctx.moveTo(65,65);
+		ctx.arc(60,65,5,0,Math.PI*2,true);  // Left eye
+		ctx.moveTo(95,65);
+		ctx.arc(90,65,5,0,Math.PI*2,true);  // Right eye
+		ctx.stroke();
 	});
 
 	THREE.Mesh.call( this, geometry, this.material );
