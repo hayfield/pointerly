3f1cc5b6ac84dc1c452b50ee764f49269c574f6b
hayfield <hayfield@gmx.com>
2012-10-12 12:55:34 +0100

    Move the code to detect the clicked thing into its own function which then works based on a passed position

--- src/js/environment/ClickHandler.js
+++ src/js/environment/ClickHandler.js
@@ -13,24 +13,24 @@ Pointerly.ClickHandler.Mouse = function( environment, setup ){
  		event.preventDefault();
  		console.log(Pointerly.CURRENT_ENVIRONMENT, event);
 
- 		// Modified http://mrdoob.github.com/three.js/examples/canvas_interactive_cubes.html
- 		var vector = new THREE.Vector3( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1, 0.5 );
-		var projector = new THREE.Projector();
-		var camera = Pointerly.CURRENT_ENVIRONMENT.camera;
-		projector.unprojectVector( vector, camera );
-
-		var ray = new THREE.Ray( camera.position, vector.subSelf( camera.position ).normalize() );
-		
-		var intersects = ray.intersectObjects( Pointerly.CURRENT_ENVIRONMENT.objects() );
-		
-		if ( intersects.length > 0 ) {
-			intersects[ 0 ].object.material.color.setHex( Math.random() * 0xffffff );
-		}
+		Pointerly.ClickHandler.GetClickedShape({ x: event.clientX, y: event.clientY }, Pointerly.CURRENT_ENVIRONMENT); 		
 	};
 
 	document.addEventListener( 'mousedown', clickHandler, false );
 };
 
-Pointerly.ClickHandler.GetClickedShape = function( clickPosition, environment, setup ){
-	console.log(clickPosition);
+Pointerly.ClickHandler.GetClickedShape = function( clickPosition, environment ){
+	// Modified http://mrdoob.github.com/three.js/examples/canvas_interactive_cubes.html
+	var vector = new THREE.Vector3( ( clickPosition.x / window.innerWidth ) * 2 - 1, - ( clickPosition.y / window.innerHeight ) * 2 + 1, 0.5 );
+	var projector = new THREE.Projector();
+	var camera = environment.camera;
+	projector.unprojectVector( vector, camera );
+
+	var ray = new THREE.Ray( camera.position, vector.subSelf( camera.position ).normalize() );
+	
+	var intersects = ray.intersectObjects( environment.objects() );
+	
+	if ( intersects.length > 0 ) {
+		intersects[ 0 ].object.material.color.setHex( Math.random() * 0xffffff );
+	}
 };
