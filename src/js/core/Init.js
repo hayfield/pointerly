130bb4436898c4d2401085189fc25a143420bf74
hayfield <hayfield@gmx.com>
2012-10-12 14:14:13 +0100

    Change the aspect ratio upon window resize so that things don't stretch or squash when the aspect ratio changes

--- src/js/core/Init.js
+++ src/js/core/Init.js
@@ -25,5 +25,7 @@ Pointerly.Init = function(){
 window.onresize = function(){
 	if( Pointerly.CURRENT_ENVIRONMENT instanceof Pointerly.Environment ){
 		Pointerly.CURRENT_ENVIRONMENT.renderer.setSize( window.innerWidth, window.innerHeight );
+		Pointerly.CURRENT_ENVIRONMENT.camera.aspect = window.innerWidth / window.innerHeight;
+		Pointerly.CURRENT_ENVIRONMENT.camera.updateProjectionMatrix();
 	}
 }
