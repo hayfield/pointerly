d89498830b0f94098f3a35fbeabd42f80a37b964
hayfield <hayfield@gmx.com>
2012-10-12 11:32:14 +0100

    Add a separate object to perform a range of basic initialisation of shapes

---
+++ src/js/display/ShapeGeneration.js
@@ -0,0 +1,26 @@
+Pointerly.ShapeGeneration = Pointerly.ShapeGeneration || {};
+
+Pointerly.ShapeGeneration.FromString = function( type, environment, setup ){
+	switch( type ){
+		case '2DGrid':
+		case 'Grid2D':
+			Pointerly.ShapeGeneration.Grid2D( environment, setup );
+			break;
+	}
+};
+
+Pointerly.ShapeGeneration.Grid2D = function( environment, setup ){
+	for( var i = 0; i < setup.rowsOfShapes; i++ ){
+		environment.shapes[i] = [];
+		for( var j = 0; j < setup.columnsOfShapes; j++ ){
+			environment.shapes[i].push(new setup.shapes[Math.floor(Math.random()*setup.shapes.length)]({
+				width: 200,
+				height: 200,
+				position: new THREE.Vector3( 200*i, 0, 200*j ),
+				color: setup.colors[Math.floor(Math.random()*setup.colors.length)]
+			}));
+
+			environment.scene.addObject( environment.shapes[i][j] );
+		}
+	}
+};
