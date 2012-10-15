daf26abc1e7532f1a2988352bafa6370f69540f8
hayfield <hayfield@gmx.com>
2012-10-15 18:35:32 +0100

    Re-render things when the canvas is resized

--- src/js/core/Init.js
+++ src/js/core/Init.js
@@ -20,5 +20,6 @@ Pointerly.Init = function(){
 window.onresize = function(){
 	if( Pointerly.CURRENT_ENVIRONMENT instanceof Pointerly.Environment ){
 		Pointerly.CURRENT_ENVIRONMENT.renderer.setSize( window.innerWidth, window.innerHeight );
+		Pointerly.CURRENT_ENVIRONMENT.renderer.render( Pointerly.CURRENT_ENVIRONMENT.scene );
 	}
 }
