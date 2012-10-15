64a67733f05a950eae666a897b7ae058a27670c8
hayfield <hayfield@gmx.com>
2012-10-15 18:19:18 +0100

    Add a function to convert an obj to a vector2

--- src/js/core/Utils.js
+++ src/js/core/Utils.js
@@ -12,6 +12,17 @@ Pointerly.Utils.ObjToVector3 = function( obj ){
 	return new THREE.Vector3( x, y, z );
 };
 
+Pointerly.Utils.ObjToVector2 = function( obj ){
+	if( typeof obj !== 'object' ){
+		return new Pointerly.Vector2( 0, 0 );
+	}
+
+	var x = typeof obj.x === 'number' ? obj.x : 0,
+		y = typeof obj.y === 'number' ? obj.y : 0;
+
+	return new Pointerly.Vector2( x, y );
+};
+
 Pointerly.Vector3 = function( x, y, z ){
 	THREE.Vector3.call( this, x, y, z );
 };
