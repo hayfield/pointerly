c0c0d15ea5d0517dd31348936eaa5bf028921e95
hayfield <hayfield@gmx.com>
2012-10-15 18:19:48 +0100

    When converting an obj to a vector3, return a Pointerly one rather than a THREE one

--- src/js/core/Utils.js
+++ src/js/core/Utils.js
@@ -2,14 +2,14 @@ Pointerly.Utils = Pointerly.Utils || {};
 
 Pointerly.Utils.ObjToVector3 = function( obj ){
 	if( typeof obj !== 'object' ){
-		return new THREE.Vector3( 0, 0, 0 );
+		return new Pointerly.Vector3( 0, 0, 0 );
 	}
 
 	var x = typeof obj.x === 'number' ? obj.x : 0,
 		y = typeof obj.y === 'number' ? obj.y : 0,
 		z = typeof obj.z === 'number' ? obj.z : 0;
 
-	return new THREE.Vector3( x, y, z );
+	return new Pointerly.Vector3( x, y, z );
 };
 
 Pointerly.Utils.ObjToVector2 = function( obj ){
