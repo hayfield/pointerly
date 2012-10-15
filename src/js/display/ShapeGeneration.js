0f8b98982d5168c3263fc7b7b7483caae654c0e1
hayfield <hayfield@gmx.com>
2012-10-15 20:40:18 +0100

    Add the code to actually implement customisation of the size and padding of shapes in a grid

--- src/js/display/ShapeGeneration.js
+++ src/js/display/ShapeGeneration.js
@@ -23,15 +23,16 @@ Pointerly.ShapeGeneration.Grid2D = function( environment, setup ){
 	var setup = setup || {},
 		shapeWidth = setup.shapeWidth || 200,
 		shapeHeight = setup.shapeHeight || 200,
-		shapePaddingHorizontal = setup.shapePaddingHorizontal || 0,
+		shapePaddingHorizontal = setup.shapePaddingHorizontal || 10,
 		shapePaddingVertical = setup.shapePaddingVertical || 0;
+	
 	for( var row = 0; row < setup.rowsOfShapes; row++ ){
 		environment.shapes[row] = [];
 		for( var col = 0; col < setup.columnsOfShapes; col++ ){
 			environment.shapes[row].push(new setup.shapes[Math.floor(Math.random()*setup.shapes.length)]({
-				width: 150,
-				height: 150,
-				position: new Pointerly.Vector2( 200*col, 200*row ),
+				width: shapeWidth,
+				height: shapeHeight,
+				position: new Pointerly.Vector2( (shapeWidth+shapePaddingHorizontal)*col, (shapeHeight+shapePaddingVertical)*row ),
 				color: setup.colors[Math.floor(Math.random()*setup.colors.length)]
 			}));
 
