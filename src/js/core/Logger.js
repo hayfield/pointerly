3df9b4d85b359980943cb2425ff915e3e5fc2af0
hayfield <hayfield@gmx.com>
2012-10-16 09:36:48 +0100

    Add some basic stuff to logger so that it can track things

--- src/js/core/Logger.js
+++ src/js/core/Logger.js
@@ -1,3 +1,14 @@
-Pointerly.Logger = function(){
-	
+Pointerly.Logger = function( loggerSetup ){
+	var logger = this,
+		setup = loggerSetup;
+
+	this.data = {};
+
+	this.resetData = function( reallySure ){
+		if( reallySure === true ){
+			logger.data = {};
+			logger.data.startTime = Date.now();
+			logger.data.lastCheck = logger.data.startTime;
+		}
+	};
 };
