6d7794ec47773cd36698df3345954648da4da20e
hayfield <hayfield@gmx.com>
2012-11-13 14:31:32 +0000

    Get the home position displaying nicely

--- src/js/core/Home.js
+++ src/js/core/Home.js
@@ -1,9 +1,9 @@
 Pointerly.Home = function(){
 	this.domElement = document.createElement('div');
 
-	this.domElement.width = 100;
-	this.domElement.height = 100;
-	this.domElement.backgroundColor = 'blue';
+	this.domElement.style.width = '100px';
+	this.domElement.style.height = '100px';
+	this.domElement.style.backgroundColor = 'blue';
 	this.domElement.id = 'homePosition';
 
 	document.body.appendChild( this.domElement );
