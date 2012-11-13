4fce516c98d1505543b4b44d1a397e356d37197e
hayfield <hayfield@gmx.com>
2012-11-13 14:40:55 +0000

    Get the home position nicely located on the screen ontop of the canvas

--- src/js/core/Home.js
+++ src/js/core/Home.js
@@ -1,10 +1,21 @@
 Pointerly.Home = function(){
+	var home = this;
+
 	this.domElement = document.createElement('div');
 
 	this.domElement.style.width = '100px';
 	this.domElement.style.height = '100px';
 	this.domElement.style.backgroundColor = 'blue';
-	this.domElement.id = 'homePosition';
+	this.domElement.className += 'homePosition';
+
+	this.setPosition = function( x, y ){
+
+	};
+
+	this.setSize = function( width, height ){
+		home.domElement.style.width = width;
+		home.domElement.style.height = height;
+	};
 
 	document.body.appendChild( this.domElement );
 };
