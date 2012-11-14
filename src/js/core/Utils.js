7239ff0691a4331f2d17e1b2243cb03ed692aac5
hayfield <hayfield@gmx.com>
2012-11-14 18:39:12 +0000

    Remove the Pointerly Vector3 since it isn't used anywhere any more

--- src/js/core/Utils.js
+++ src/js/core/Utils.js
@@ -1,17 +1,5 @@
 Pointerly.Utils = Pointerly.Utils || {};
 
-Pointerly.Utils.ObjToVector3 = function( obj ){
-	if( typeof obj !== 'object' ){
-		return new Pointerly.Vector3( 0, 0, 0 );
-	}
-
-	var x = typeof obj.x === 'number' ? obj.x : 0,
-		y = typeof obj.y === 'number' ? obj.y : 0,
-		z = typeof obj.z === 'number' ? obj.z : 0;
-
-	return new Pointerly.Vector3( x, y, z );
-};
-
 Pointerly.Utils.ObjToVector2 = function( obj ){
 	if( typeof obj !== 'object' ){
 		return new Pointerly.Vector2( 0, 0 );
@@ -21,12 +9,6 @@ Pointerly.Utils.ObjToVector2 = function( obj ){
 		y = typeof obj.y === 'number' ? obj.y : 0;
 
 	return new Pointerly.Vector2( x, y );
-};
-
-Pointerly.Vector3 = function( x, y, z ){
-	this.x = x || 0;
-	this.y = y || 0;
-	this.z = z || 0;
 };
 
 Pointerly.Vector2 = function( x, y ){
