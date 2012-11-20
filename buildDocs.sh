7d7ac641b3063a137d3fb4dd23dc204aa2645749
hayfield <hayfield@gmx.com>
2012-11-20 17:43:49 +0000

    Get the docs builder to include private variables

--- buildDocs.sh
+++ buildDocs.sh
@@ -1,2 +1,2 @@
 # requires jsdoc-toolkit from http://code.google.com/p/jsdoc-toolkit/ to be located in the jsdoc-toolkit/ folder
-java -jar jsdoc-toolkit/jsrun.jar jsdoc-toolkit/app/run.js -a -t=jsdoc-toolkit/templates/jsdoc -d=docs/ -r src/js/ 
+java -jar jsdoc-toolkit/jsrun.jar jsdoc-toolkit/app/run.js -a -p -t=jsdoc-toolkit/templates/jsdoc -d=docs/ -r src/js/ 
