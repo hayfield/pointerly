9f6b3faef35794520cb0c15f71810e91457df130
hayfield <hayfield@gmx.com>
2012-11-20 17:38:21 +0000

    Add docs to the functions in Pointerly.Home

--- src/js/core/Home.js
+++ src/js/core/Home.js
@@ -1,3 +1,15 @@
+/**
+	A Home Position that resides in the middle of the screen
+	@constructor
+	@param {object} homeSetup The setup for the home position.<br />
+		If <code>undefined</code>, setup is not completed.<br />
+		Should have the following properties:
+		<ul>
+			<li><code>width</code>
+			<li><code>height</code>
+			<li><code>color</code>
+		</ul>
+*/
 Pointerly.Home = function( homeSetup ){
 	var home = this,
 		width,
@@ -8,6 +20,11 @@ Pointerly.Home = function( homeSetup ){
 		return;
 	}
 
+	/**
+		Sets the position of the Home Position and logs the location
+		@param {number} x The x position in px from the left of the screen
+		@param {number} y The y position in px from the top of the screen
+	*/
 	this.setPosition = function( x, y ){
 		home.domElement.style.left = x + 'px';
 		home.domElement.style.top = y + 'px';
@@ -15,6 +32,11 @@ Pointerly.Home = function( homeSetup ){
 		Pointerly.CURRENT_ENVIRONMENT.logger.logHomeAreaPosition( x, y );
 	};
 
+	/**
+		Sets the size of the Home Position and logs the new size
+		@param {number} newWidth The new width in px
+		@param {number} newHeight The new height in px
+	*/
 	this.setSize = function( newWidth, newHeight ){
 		width = newWidth;
 		height = newHeight;
@@ -36,6 +58,10 @@ Pointerly.Home = function( homeSetup ){
 
 	document.body.appendChild( this.domElement );
 
+	/**
+		A function which fires when the mouse enters the home area.
+		It will call an <code>onEnter()</code> function if provided as a property of <code>homeSetup</code>.
+	*/
 	var onHomeEnter = function(){
 		Pointerly.CURRENT_ENVIRONMENT.logger.logHomeAreaEnter();
 		if( typeof homeSetup.onEnter === 'function' ){
@@ -43,6 +69,10 @@ Pointerly.Home = function( homeSetup ){
 		}
 	};
 
+	/**
+		A function which fires when the mouse exits the home area.
+		It will call an <code>onExit()</code> function if provided as a property of <code>homeSetup</code>.
+	*/
 	var onHomeExit = function(){
 		Pointerly.CURRENT_ENVIRONMENT.logger.logHomeAreaExit();
 		if( typeof homeSetup.onExit === 'function' ){
