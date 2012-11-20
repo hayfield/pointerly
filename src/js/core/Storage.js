155020ca927cf85066cd18d63e63c1827e13e1fa
hayfield <hayfield@gmx.com>
2012-11-20 19:01:17 +0000

    Add docs to the Storage thing

--- src/js/core/Storage.js
+++ src/js/core/Storage.js
@@ -1,15 +1,36 @@
+/**
+	A way of storing or logging data
+	@constructor
+*/
 Pointerly.Storage = function(){
 	var storage = this;
 
-	this.requestedSpace = 1024*1024*1024;
+	/**
+		The minimum amount of storage space we desire, in bytes
+		@constant
+		@type number
+	*/
+	this.REQUESTED_SPACE = 1024*1024*1024;
 
+	/**
+		The type of storage to use
+		@constant
+	*/
 	this.TYPE = window.TEMPORARY;
 
+	/**
+		The buffer of items to be stored
+		@type Array
+	*/
 	this._writeBuffer = [];
 
 	window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
 	window.storageInfo = window.storageInfo || window.webkitStorageInfo;
 
+	/**
+		A general error handler for dealing with file storage
+		@param {FileError} e The error which occured
+	*/
 	this.errorHandler = function( e ){
 		var msg = '';
 
@@ -40,6 +61,9 @@ Pointerly.Storage = function(){
 		console.log('Storage Error: ' + msg);
 	};
 
+	/**
+		Finds and displays the quotas for different types of storage
+	*/
 	this.displayQuotas = function(){
 		window.storageInfo.queryUsageAndQuota(window.PERSISTENT, function(used, total){
 			console.log('persistent storage:', used, 'total storage:', total, 'remaining:', total-used);
@@ -50,6 +74,9 @@ Pointerly.Storage = function(){
 		});
 	};
 
+	/**
+		Initialises the storage, requesting any required quotas and creating the file systems
+	*/
 	this.initStorage = function(){
 		var onInitFs = function( fs ){
 			storage.fs = fs;
@@ -66,10 +93,14 @@ Pointerly.Storage = function(){
 				console.log('Error requesting quota', e);
 			});
 		} else if( storage.TYPE === window.TEMPORARY ){
-			window.requestFileSystem(storage.TYPE, storage.requestedSpace, onInitFs, storage.errorHandler);
+			window.requestFileSystem(storage.TYPE, storage.REQUESTED_SPACE, onInitFs, storage.errorHandler);
 		}
 	};
 
+	/**
+		Writes the first item in the buffer to the given file. If the buffer is empty, writes the empty string
+		@param {FileEntry} fileEntry The file to write to
+	*/
 	this.writeBufferToFile = function( fileEntry ){
 		console.log(fileEntry, fileEntry.name);
 
@@ -92,6 +123,13 @@ Pointerly.Storage = function(){
 		}, storage.errorHandler);
 	};
 
+	/**
+		Finds the next numbered file with the name and type specified so that overwriting does not occur.
+		When the file is found, the callback is executed and given the file.
+		@param {string} name The name of the file
+		@param {string} type The file type
+		@param {function} callback The callback to execute once the file is found
+	*/
 	this.getNextNumberedFile = function( name, type, callback ){
 		var id = 0;
 
@@ -113,6 +151,11 @@ Pointerly.Storage = function(){
 		getFile();
 	};
 
+	/**
+		Saves data to a file with the given name
+		@param {string} name The name of the file to write to, not including file type
+		@param data The data to write to file
+	*/
 	this.save = function( name, data ){
 		console.log('saving');
 		storage._writeBuffer.push(data);
@@ -120,6 +163,11 @@ Pointerly.Storage = function(){
 		storage.getNextNumberedFile( name, 'txt', storage.writeBufferToFile );
 	};
 
+	/**
+		Remove all data that has been stored
+		@param {boolean} [reallySure] Needs to be set to <code>true</code> to actually remove the data
+		@param {string} [superDuperSure] Needs to be set to <code>Yes, I really want to delete EVERYTHING!!</code> to actually remove the data
+	*/
 	this.removeAllData = function( reallySure, superDuperSure ){
 		if( reallySure && superDuperSure === 'Yes, I really want to delete EVERYTHING!!' && storage.dir ){
 			storage.dir.removeRecursively(function(){
@@ -130,7 +178,7 @@ Pointerly.Storage = function(){
 	};
 
 	window.storageInfo.queryUsageAndQuota(window.TEMPORARY, function(used, total){
-		if( total < storage.requestedSpace || (total-used) < (storage.requestedSpace/2) ){
+		if( total < storage.REQUESTED_SPACE || (total-used) < (storage.REQUESTED_SPACE/2) ){
 			console.warn('Not a lot of temorary space available!\n',
 				'used:', used, 'total:', total, 'remaining:', total-used);
 		}
