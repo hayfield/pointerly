e8df250428076e4feb324f60e4392a9e02269403
hayfield <hayfield@gmx.com>
2012-11-20 18:38:08 +0000

    Adds docs to the Renderer

--- src/js/core/Renderer.js
+++ src/js/core/Renderer.js
@@ -1,10 +1,34 @@
+/**
+	A renderer that will display a Pointerly scene
+	@constructor
+*/
 Pointerly.Renderer = function(){
-	var canvas = document.createElement('canvas'),
-		ctx = canvas.getContext('2d'),
-		canvasWidth, canvasHeight;
+	/**
+		The canvas that the renderer inserts into the DOM
+	*/
+	var canvas = document.createElement('canvas');
+	/**
+		The context on which drawing will occur
+	*/
+	var ctx = canvas.getContext('2d');
+	/**
+		The width of the canvas
+		@type number
+	*/
+	var canvasWidth;
+	/**
+		The height of the canvas
+		@type number
+	*/
+	var canvasHeight;
 
 	this.domElement = canvas;
 
+	/**
+		Sets the size of the renderer
+		@param {number} width The width of the renderer
+		@param {number} height The height of the renderer
+	*/
 	this.setSize = function( width, height ){
 		canvasWidth = width;
 		canvasHeight = height;
@@ -13,6 +37,10 @@ Pointerly.Renderer = function(){
 		Pointerly.CURRENT_ENVIRONMENT.logger.logCanvasResize( width, height );
 	};
 
+	/**
+		Renders a scene onto the renderer's context
+		@param {Pointerly.Scene} scene The scene to render
+	*/
 	this.render = function( scene ){
 		ctx.clearRect( 0, 0, canvasWidth, canvasHeight );
 		scene.objects.forEach(function( obj ){
