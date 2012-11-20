8bf38ea458b7e0385f2553c8c8e0c1e6698bd1ef
hayfield <hayfield@gmx.com>
2012-11-20 18:46:00 +0000

    Add documentation to the Scene:

--- src/js/core/Scene.js
+++ src/js/core/Scene.js
@@ -1,13 +1,29 @@
+/**
+	A scene which contains things
+	@constructor
+*/
 Pointerly.Scene = function(){
+	/**
+		The objects which exist in a scene
+		@type Array
+	*/
 	this.objects = [];
 };
 
+/**
+	If an object does not already exist within the scene, add it to the scene
+	@param obj The object to add to the scene
+*/
 Pointerly.Scene.prototype.addObject = function( obj ) {
 	if( this.objects.indexOf(obj) === -1 ){
 		this.objects.push(obj);
 	}
 };
 
+/**
+	If an object is within the scene, remove it
+	@param obj The object to remove
+*/
 Pointerly.Scene.prototype.removeObject = function( obj ){
 	var idx = this.objects.indexOf( obj );
 
@@ -16,6 +32,9 @@ Pointerly.Scene.prototype.removeObject = function( obj ){
 	}
 };
 
+/**
+	Removes all objects from the scene
+*/
 Pointerly.Scene.prototype.removeAllObjects = function(){
 	this.objects = [];
 };
