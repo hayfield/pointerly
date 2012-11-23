df2320e65c37ce4c39a450334492a1cb13507c7d
hayfield <hayfield@gmx.com>
2012-11-23 11:22:29 +0000

    Remove the 'Experiment' Shape Generation method

--- src/js/display/ShapeGeneration.js
+++ src/js/display/ShapeGeneration.js
@@ -27,7 +27,6 @@ Pointerly.ShapeGeneration.SetupDefaults = function(){
 	Pointerly.ShapeGeneration.Register( 'grid2d', Pointerly.ShapeGeneration.Grid2D );
 	Pointerly.ShapeGeneration.Register( '2dgrid', Pointerly.ShapeGeneration.Grid2D );
 	Pointerly.ShapeGeneration.Register( 'grid', Pointerly.ShapeGeneration.Grid2D );
-	Pointerly.ShapeGeneration.Register( 'experiment', Pointerly.ShapeGeneration.Experiment );
 };
 
 /**
@@ -65,19 +64,4 @@ Pointerly.ShapeGeneration.Grid2D = function( environment, setup ){
 			}), row, col);
 		}
 	}
-};
-
-Pointerly.ShapeGeneration.Experiment = function( environment, setup ){
-	environment.addShape(new Pointerly.Shapes.Rectangle({
-		width: 40,
-		height: environment.renderer.domElement.height,
-		color: 'black',
-		position: new Pointerly.Vector2( environment.renderer.domElement.width / 4, 0 )
-	}));
-	environment.addShape(new Pointerly.Shapes.Rectangle({
-		width: 40,
-		height: 40,
-		color: 'black',
-		position: new Pointerly.Vector2( environment.renderer.domElement.width / 2 - 20, environment.renderer.domElement.height / 2 - 20 )
-	}));
 };
