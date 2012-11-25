14854e869d60edb685dc44cbcc57b789c2bcf474
hayfield <hayfield@gmx.com>
2012-11-25 19:27:04 +0000

    Position the home position in the middle of the screen and shapes at the correct position on either side

--- src/js/core/Init.js
+++ src/js/core/Init.js
@@ -19,7 +19,7 @@ Pointerly.onresize = function(){
 	if( Pointerly.CURRENT_ENVIRONMENT instanceof Pointerly.Environment ){
 		var home = Pointerly.CURRENT_ENVIRONMENT.homePosition;
 		if( home.domElement ){
-			home.setPosition( window.innerWidth / 2, window.innerHeight / 2 );
+			home.setPosition( (window.innerWidth - parseInt(home.domElement.style.width, 10)) / 2, (window.innerHeight - parseInt(home.domElement.style.height, 10)) / 2 );
 		}
 		Pointerly.CURRENT_ENVIRONMENT.renderer.setSize( window.innerWidth, window.innerHeight );
 		Pointerly.CURRENT_ENVIRONMENT.render();
